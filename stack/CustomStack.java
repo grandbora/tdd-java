42c5089acdfe402e5c2c3e49b0e63c4729dbb69d
grandbora <bora.tunca@yahoo.com>
2013-02-10 14:00:59 +0100

    stack impelemented

--- stack/CustomStack.java
+++ stack/CustomStack.java
@@ -1,9 +1,38 @@
 package stack;
 
-public class CustomStack {
+import java.util.Arrays;
 
-	public Boolean isEmpty() {
-		return true;
+public class CustomStack<T> {
+
+	Object[] elementList = new Object[0];
+
+	public Boolean empty() {
+		return 0 == elementList.length;
 	}
 
+	public void push(T o) {
+		elementList = Arrays.copyOf(elementList, elementList.length + 1);
+		elementList[elementList.length - 1] = o;
+	}
+
+	@SuppressWarnings("unchecked")
+	public T pop() {
+		T returnValue = (T) elementList[elementList.length - 1];
+		elementList = Arrays.copyOf(elementList, elementList.length - 1);
+		return returnValue;
+	}
+
+	@SuppressWarnings("unchecked")
+	public T peek() {
+		return (T) elementList[elementList.length - 1];
+	}
+
+	public int search(T o) {
+		for (int i = elementList.length - 1; i > -1; i--) {
+			if (elementList[i].equals(o))
+				return elementList.length - 1 - i;
+		}
+
+		return -1;
+	}
 }
--- stack/TestCustomStack.java
+++ stack/TestCustomStack.java
@@ -28,8 +28,123 @@ public class TestCustomStack {
 
 	@Test
 	public void testIsMepty() {
-		CustomStack customStack = new CustomStack();
-		assertTrue(customStack.isEmpty());
+		CustomStack<String> customStack = new CustomStack<String>();
+		assertTrue(customStack.empty());
+		customStack.push("element1");
+		assertFalse(customStack.empty());
+		customStack.pop();
+		assertTrue(customStack.empty());
 	}
 
+	@Test
+	public void testPushPop() {
+		String el0 = "firstElement0";
+		String el1 = "firstElement1";
+		String el2 = "firstElement2";
+		String el3 = "firstElement3";
+
+		CustomStack<String> customStack = new CustomStack<String>();
+
+		customStack.push(el0);
+		assertSame(el0, customStack.pop());
+		customStack.push(el0);
+		customStack.push(el0);
+		assertSame(el0, customStack.pop());
+		customStack.push(el1);
+		customStack.push(el2);
+		customStack.push(el3);
+		customStack.push(el1);
+		assertSame(el1, customStack.pop());
+		assertSame(el3, customStack.pop());
+		assertSame(el2, customStack.pop());
+		assertSame(el1, customStack.pop());
+		assertSame(el0, customStack.pop());
+		assertTrue(customStack.empty());
+	}
+
+	@Test
+	public void testPeek() {
+		String el0 = "firstElement0";
+		String el1 = "firstElement1";
+		String el2 = "firstElement2";
+		String el3 = "firstElement3";
+
+		CustomStack<String> customStack = new CustomStack<String>();
+
+		customStack.push(el0);
+		assertSame(el0, customStack.peek());
+		customStack.push(el1);
+		assertSame(el1, customStack.peek());
+		customStack.push(el2);
+		assertSame(el2, customStack.peek());
+		customStack.push(el3);
+		assertSame(el3, customStack.peek());
+		customStack.push(el1);
+		assertSame(el1, customStack.peek());
+		customStack.pop();
+		assertSame(el3, customStack.peek());
+		customStack.pop();
+		assertSame(el2, customStack.peek());
+		customStack.pop();
+		assertSame(el1, customStack.peek());
+		customStack.pop();
+		assertSame(el0, customStack.peek());
+	}
+	
+	public void testSearchOnEmpryStack() {
+		CustomStack<String> customStack = new CustomStack<String>();
+		assertSame(-1, customStack.search("ahoyARandomString"));
+	}
+
+	
+	@Test
+	public void testSearch() {
+		String el0 = "firstElement0";
+		String el1 = "firstElement1";
+		String el2 = "firstElement2";
+		String el3 = "firstElement3";
+
+		CustomStack<String> customStack = new CustomStack<String>();
+
+		customStack.push(el0);
+		customStack.push(el1);
+		customStack.push(el2);
+		customStack.push(el3);
+		
+		assertSame(3, customStack.search(el0));
+		assertSame(2, customStack.search(el1));
+		assertSame(1, customStack.search(el2));
+		assertSame(0, customStack.search(el3));
+		assertSame(-1, customStack.search("nonExisting1"));
+		
+		customStack.push(el3);
+		customStack.push(el2);
+		customStack.push(el0);
+		customStack.push(el1);
+		
+		assertSame(1, customStack.search(el0));
+		assertSame(0, customStack.search(el1));
+		assertSame(2, customStack.search(el2));
+		assertSame(3, customStack.search(el3));
+		assertSame(-1, customStack.search("nonExisting2"));
+		
+		customStack.pop();
+		customStack.pop();
+		
+		assertSame(5, customStack.search(el0));
+		assertSame(4, customStack.search(el1));
+		assertSame(0, customStack.search(el2));
+		assertSame(1, customStack.search(el3));
+		assertSame(-1, customStack.search("nonExisting3"));
+		
+		customStack.pop();
+		customStack.pop();
+		customStack.pop();
+		customStack.pop();
+		
+		assertSame(1, customStack.search(el0));
+		assertSame(0, customStack.search(el1));
+		assertSame(-1, customStack.search(el2));
+		assertSame(-1, customStack.search(el3));
+	}
 }
